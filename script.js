8dc7da3feb8c18ea31669f319923cd2fc36de2af
fatimalikova <fatimakhm@code.edu.az>
2026-03-01 00:50:50 +0400

    Operation in objects

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
+"use strict";
+
 // window.alert("Hello World!");
 // console.log("Hello!");
 // prompt("What is your name?");
 // console.log(document);
 // document.writeln("<h1 style='color: blue;'>Hello World!</h1>");
 // document.getElementById("text").innerText = "Hello Fatima!";
-//lete,var,const
+//let,var,const
 // var num = 12;
 // let word = "Hello";
 // const pi = 3.14;
@@ -77,19 +79,93 @@
 
 //function yaratmaq yollari=> function declaration, function expression, arrow function
 // function declaration
-function Print(message) {
-    console.log("Hello " + message);
-}
-Print("Fatima!");
+// function Print(message) {
+//     console.log("Hello " + message);
+// }
+// Print("Fatima!");
 
-// function expression
-const Print2 = function (message) {
-    console.log("Hello " + message);
-}
-Print2("Fatima!");
+// // function expression (yuxarda caxirmaq olmaz)
+// const Print2 = function (message) {
+//     console.log("Hello " + message);
+// }
+// Print2("Fatima!");
 
-//arrow function
-const Print3 = (message) => {
-    console.log("Hello " + message);
+// //arrow function (yuxarda caxirmaq olmaz)
+// const Print3 = message => console.log("Hello " + message);
+// Print3("Fatima!");
+
+// function Sum(a, b) {
+//     return a + b;
+// }
+// try{
+//     if(isNaN(Sum(12))){
+//         throw new Error("Invalid input! Both parameters should be numbers.");
+//     }
+// }
+// catch(error){
+//     console.log(error.message);
+// }
+
+//parametr sayi bilinmediyi halda arguments keywordu istifade olunur
+// function SumArrayElements(){
+//     let array = Array.from(arguments);
+//     let sum =0;
+//     for(let i=0;i<array.length;i++){
+//         const element = array[i];
+//         sum+=element;
+//     }
+//     return sum; 
+// }
+// console.log(SumArrayElements(1,2,3,5));
+
+// function Fullname(firstname,lastname){
+//     return {
+//         firstname:firstname,
+//         lastname:lastname,
+//     }
+// }
+// console.log(Fullname("Fatima","Malikova"));
+
+// let n = num => num>6;
+// console.log(n(12));
+
+// debugger;
+// let name ="Fatima";
+// let surname="Malikova";
+// let fullname= `${name} ${surname}`;
+// console.log(fullname);
+
+
+const user ={
+    username:"_user123",
+    password:"_1234",
+    age:67,
+    address:"Baku",
+    hobbies:["reading","traveling","cooking"],
+
+    getAge:function(){
+        return user.age;//this.age
+    },
+    getuserName:()=>{
+        return this.username;
+    }
 }
-Print3("Fatima!");
+Object.defineProperty(user,"email",{value:"fatima@gmail.com",writable:true});
+console.log(user.email);
+// console.log(user["age"]);
+// console.log(user.age);
+
+// for (const key in user) {
+//     console.log(user[key]);
+// }
+
+// console.log(user.getuserName());
+
+// console.log(Object.keys(user));
+// console.log(Object.values(user));
+// console.log(Object.entries(user));
+// delete user.password;//password silindi
+// console.log(user);
+// Object.freeze(user);//deyisiklik etmek olmaz bu line dan sonra
+// user.address="Ganja";
+// console.log(user);
